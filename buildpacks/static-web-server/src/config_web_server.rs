10047672efd6e5d20b2c4f5c06e9ebc26a739d82
Mars Hall <mars@heroku.com>
2024-08-16 17:26:51 -0700

    WIP config web server using project.toml

--- buildpacks/static-web-server/src/config_web_server.rs
+++ buildpacks/static-web-server/src/config_web_server.rs
@@ -4,6 +4,13 @@ use crate::{StaticWebServerBuildpack, StaticWebServerBuildpackError};
 use libcnb::layer::{LayerRef};
 use libcnb::{build::BuildContext, layer::UncachedLayerDefinition};
 use libcnb::data::layer_name;
+use libcnb::read_toml_file;
+use libherokubuildpack::log::log_info;
+use libherokubuildpack::toml::toml_select_value;
+use serde::{Deserialize, Serialize};
+use toml::toml;
+
+const DEFAULT_DOC_ROOT: &str = "public";
 
 pub(crate) fn config_web_server(
     context: &BuildContext<StaticWebServerBuildpack>,
@@ -19,32 +26,93 @@ pub(crate) fn config_web_server(
         },
     )?;
 
-    let default_caddy_config = r#"
-    {
-        "apps": {
-            "http": {
-                "servers": {
-                    "public": {
-                        "listen": [":{env.PORT}"],
-                        "routes": [
-                            {
-                                "handle": [
-                                    {
-                                        "handler": "file_server",
-                                        "root": "public/"
-                                    }
-                                ]
+    let project_toml_data = read_toml_file(context.app_dir.join("project.toml"))
+        .map_err(StaticWebServerBuildpackError::ReadTOMLFile)?;
+    let project_toml: toml::Value = project_toml_data;
+
+    let doc_root = toml_select_value(
+        vec!["_", "metadata", "web-server", "root"], 
+        &project_toml)
+        .and_then(toml::Value::as_str)
+        .unwrap_or(DEFAULT_DOC_ROOT);
+
+    let caddy_config = CaddyConfig {
+        apps: CaddyConfigApps { 
+            http: CaddyConfigAppHTTP { 
+                servers: CaddyConfigHTTPServers { 
+                    public: CaddyConfigHTTPServerPublic { 
+                        listen: vec![":{env.PORT}".to_owned()], 
+                        routes: vec![
+                            CaddyHTTPServerRouteHandle {
+                                handler: "file_server".to_owned(),
+                                root: doc_root.to_string(),
                             }
                         ]
                     }
                 }
             }
         }
-    }
-    "#;
+    };
 
-    fs::write(configuration_layer.path().join("caddy.json"), default_caddy_config)
+    let caddy_config_json = serde_json::to_string(&caddy_config)
+        .map_err(StaticWebServerBuildpackError::JSON)?;
+    fs::write(configuration_layer.path().join("caddy.json"), caddy_config_json)
         .map_err(StaticWebServerBuildpackError::File)?;
 
     Ok(configuration_layer)
+}
+
+// Caddy JSON config:
+// {
+//     "apps": {
+//         "http": {
+//             "servers": {
+//                 "public": {
+//                     "listen": [":{env.PORT}"],
+//                     "routes": [
+//                         {
+//                             "handle": [
+//                                 {
+//                                     "handler": "file_server",
+//                                     "root": "public/"
+//                                 }
+//                             ]
+//                         }
+//                     ]
+//                 }
+//             }
+//         }
+//     }
+// }
+
+#[derive(Serialize, Deserialize)]
+struct CaddyConfig {
+    apps: CaddyConfigApps
+}
+
+#[derive(Serialize, Deserialize)]
+struct CaddyConfigApps {
+    http: CaddyConfigAppHTTP
+}
+
+#[derive(Serialize, Deserialize)]
+struct CaddyConfigAppHTTP {
+    servers: CaddyConfigHTTPServers
+}
+
+#[derive(Serialize, Deserialize)]
+struct CaddyConfigHTTPServers {
+    public: CaddyConfigHTTPServerPublic
+}
+
+#[derive(Serialize, Deserialize)]
+struct CaddyConfigHTTPServerPublic {
+    listen: Vec<String>,
+    routes: Vec<CaddyHTTPServerRouteHandle>
+}
+
+#[derive(Serialize, Deserialize)]
+struct CaddyHTTPServerRouteHandle {
+    handler: String,
+    root: String
 }
--- buildpacks/static-web-server/src/errors.rs
+++ buildpacks/static-web-server/src/errors.rs
@@ -3,6 +3,7 @@ use commons::output::build_log::{BuildLog, Logger, StartedLogger};
 use commons::output::fmt;
 use commons::output::fmt::DEBUG_INFO;
 use indoc::formatdoc;
+use libcnb::TomlFileError;
 use std::fmt::Display;
 use std::io;
 use std::io::stdout;
@@ -16,8 +17,10 @@ locally with a minimal example and open an issue in the buildpack's GitHub repos
 
 #[derive(Debug)]
 pub(crate) enum StaticWebServerBuildpackError {
+    ReadTOMLFile(TomlFileError),
     Download(libherokubuildpack::download::DownloadError),
     File(io::Error),
+    JSON(serde_json::Error),
 }
 
 pub(crate) fn on_error(error: libcnb::Error<StaticWebServerBuildpackError>) {
@@ -32,8 +35,10 @@ pub(crate) fn on_error(error: libcnb::Error<StaticWebServerBuildpackError>) {
 
 fn on_buildpack_error(error: StaticWebServerBuildpackError, logger: Box<dyn StartedLogger>) {
     match error {
+        StaticWebServerBuildpackError::ReadTOMLFile(e) => on_toml_read_file_error(&e, logger),
         StaticWebServerBuildpackError::Download(e) => on_download_error(&e, logger),
         StaticWebServerBuildpackError::File(e) => on_build_error(&e, logger),
+        StaticWebServerBuildpackError::JSON(e) => on_json_error(&e, logger),
     }
 }
 
@@ -50,6 +55,22 @@ fn on_download_error(error: &libherokubuildpack::download::DownloadError, logger
         .announce()
         .error(&formatdoc! {"
             Unable to download the static web server for {buildpack_name}. 
+        ", buildpack_name = fmt::value(BUILDPACK_NAME) });
+}
+
+fn on_toml_read_file_error(error: &TomlFileError, logger: Box<dyn StartedLogger>) {
+    print_error_details(logger, &error)
+        .announce()
+        .error(&formatdoc! {"
+            Unable to read TOML file for {buildpack_name}. 
+        ", buildpack_name = fmt::value(BUILDPACK_NAME) });
+}
+
+fn on_json_error(error: &serde_json::Error, logger: Box<dyn StartedLogger>) {
+    print_error_details(logger, &error)
+        .announce()
+        .error(&formatdoc! {"
+            JSON error from {buildpack_name}. 
         ", buildpack_name = fmt::value(BUILDPACK_NAME) });
 }
 
