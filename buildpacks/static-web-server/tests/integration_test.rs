0ed57b048f7e2f07f856789d5f1018df1e71b30b
Mars Hall <mars@users.noreply.github.com>
2024-08-20 17:15:38 -0700

    Configurable global response headers (#3)
    * Configurable global response headers
    
    * Configurable path-matched response headers

--- buildpacks/static-web-server/tests/integration_test.rs
+++ buildpacks/static-web-server/tests/integration_test.rs
@@ -3,7 +3,7 @@
 
 use libcnb_test::{assert_contains};
 use test_support::{
-    assert_web_response, static_web_server_integration_test,
+    assert_web_response, assert_web_response_header, static_web_server_integration_test,
     wait_for, PORT,
 };
 
@@ -22,5 +22,17 @@ fn custom_doc_root() {
     static_web_server_integration_test("./fixtures/custom_doc_root", |ctx| {
         assert_contains!(ctx.pack_stdout, "Static Web Server");
         assert_web_response(&ctx, "Welcome to CNB Static Web Server Doc Root Test!");
+    });
+}
+
+#[test]
+#[ignore = "integration test"]
+fn custom_headers() {
+    static_web_server_integration_test("./fixtures/custom_headers", |ctx| {
+        assert_contains!(ctx.pack_stdout, "Static Web Server");
+        assert_web_response(&ctx, "Welcome to CNB Static Web Server Response Headers Test!");
+        assert_web_response_header(&ctx, "/", "X-Global", "Hello");
+        assert_web_response_header(&ctx, "/", "X-Only-Default", "Hiii");
+        assert_web_response_header(&ctx, "/page2.html", "X-Only-HTML", "Hi");
     });
 }
