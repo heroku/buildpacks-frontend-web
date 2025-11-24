6032b023c93f0655bdf8ac9864a1d297f71aa619
Mars Hall <mars@heroku.com>
2025-11-24 11:27:37 -0800

    Distinguish whether HTML data-* attribute injection happened; includes function renaming to clarify control flow.

--- common/env_as_html_data/src/lib.rs
+++ common/env_as_html_data/src/lib.rs
@@ -15,10 +15,10 @@ use rcdom::{Handle, Node, NodeData, RcDom, SerializableHandle};
 
 pub(crate) fn env_as_html_data() {}
 
-pub(crate) fn inject_data_into_html<S: BuildHasher>(
+pub(crate) fn parse_html_and_inject_data<S: BuildHasher>(
     data: &HashMap<String, String, S>,
     html: &str,
-) -> Result<String, Error> {
+) -> Result<(bool, String), Error> {
     let opts = ParseOpts {
         tree_builder: TreeBuilderOpts {
             ..Default::default()
@@ -30,10 +30,9 @@ pub(crate) fn inject_data_into_html<S: BuildHasher>(
         .read_from(&mut html.as_bytes())
         .map_err(|e| Error::ParseError(e.to_string()))?;
 
-    let did_write = write_attrs_into_document(data, &dom.document)?;
-    if !did_write {
-        return Err(Error::NoBodyElementError);
-    }
+    if let (_, false) = match_html_body_and_inject_data(data, &dom.document)? {
+        return Ok((false, html.to_owned()));
+    };
 
     let document: SerializableHandle = dom.document.clone().into();
 
@@ -42,20 +41,20 @@ pub(crate) fn inject_data_into_html<S: BuildHasher>(
         .map_err(|e| Error::SerializeError(e.to_string()))?;
     let output =
         std::str::from_utf8(buf.as_slice()).map_err(|e| Error::EncodeError(e.to_string()))?;
-    Ok(output.to_string())
+    Ok((true, output.to_string()))
 }
 
 #[allow(clippy::type_complexity)]
-fn write_attrs_into_document<S: BuildHasher>(
+fn match_html_body_and_inject_data<S: BuildHasher>(
     data: &HashMap<String, String, S>,
     node: &Handle,
-) -> Result<bool, Error> {
+) -> Result<(bool, bool), Error> {
     // Closure around the reference-counted HTML DOM document/nodes, to support recursing to find the body element
     struct RecurseToMatch<'r> {
-        f: &'r dyn Fn(&RecurseToMatch, &Rc<Node>) -> Result<bool, Error>,
+        f: &'r dyn Fn(&RecurseToMatch, &Rc<Node>) -> Result<(bool, bool), Error>,
     }
     let recurse_to_match = RecurseToMatch {
-        f: &|recurse_to_match: &RecurseToMatch, n: &Rc<Node>| -> Result<bool, Error> {
+        f: &|recurse_to_match: &RecurseToMatch, n: &Rc<Node>| -> Result<(bool, bool), Error> {
             let m: Option<&Rc<Node>> = match n.data {
                 NodeData::Element { ref name, .. } => {
                     if *name.local == *"body" {
@@ -71,26 +70,33 @@ fn write_attrs_into_document<S: BuildHasher>(
             if m.is_none() {
                 let mut children = n.children.borrow_mut();
                 for child in children.iter_mut() {
-                    let mm = (recurse_to_match.f)(recurse_to_match, child);
-                    if let Ok(true) = mm {
-                        return Ok(true);
-                    }
+                    match (recurse_to_match.f)(recurse_to_match, child) {
+                        Ok((true, did_inject)) => return Ok((true, did_inject)),
+                        _ => (),
+                    };
                 }
-                return Ok(false);
+                return Ok((false, false));
             }
 
-            inject_public_attrs::<S>(data, m.expect("Document Node is already known to be Some"))?;
-            Ok(true)
+            let did_inject = inject_html_data_attrs::<S>(
+                data,
+                m.expect("Document Node is already known to be Some"),
+            )?;
+            Ok((true, did_inject))
         },
     };
 
-    (recurse_to_match.f)(&recurse_to_match, node)
+    match (recurse_to_match.f)(&recurse_to_match, node) {
+        Ok((true, did_inject)) => Ok((true, did_inject)),
+        Ok((false, _)) => Err(Error::NoBodyElementError),
+        Err(e) => Err(e),
+    }
 }
 
-fn inject_public_attrs<S: BuildHasher>(
+fn inject_html_data_attrs<S: BuildHasher>(
     data: &HashMap<String, String, S>,
     element: &Rc<Node>,
-) -> Result<(), Error> {
+) -> Result<bool, Error> {
     let NodeData::Element {
         name: qual_name,
         attrs,
@@ -108,7 +114,7 @@ fn inject_public_attrs<S: BuildHasher>(
     keys.sort_unstable();
 
     if keys.is_empty() {
-        return Ok(());
+        return Ok(false);
     }
 
     for k in &keys {
@@ -124,16 +130,16 @@ fn inject_public_attrs<S: BuildHasher>(
         attrs.borrow_mut().push(new_attr);
     }
 
-    Ok(())
+    Ok(true)
 }
 
 #[cfg(test)]
 mod tests {
-    use crate::inject_data_into_html;
+    use crate::parse_html_and_inject_data;
     use std::collections::HashMap;
 
     #[test]
-    fn inject_data_into_html_sets_public_data() {
+    fn parse_html_and_inject_data_sets_public_data() {
         let mut data: HashMap<String, String> = HashMap::new();
         data.insert(
             "PUBLIC_API_URL".to_string(),
@@ -144,17 +150,19 @@ mod tests {
             "NOT_PUBLIC_VAR".to_string(),
             "non-public should not be included".to_string(),
         );
-        let html_missing_body =
+        let html =
             "<html><head><title>Hello World</title></head><body><h1>Hello World</h1></body></html>";
         let expected_html = r#"<html><head><title>Hello World</title></head><body data-public_api_url="https://api.example.com/v1" data-public_release_version="v101"><h1>Hello World</h1></body></html>"#;
-        let result = inject_data_into_html(&data, html_missing_body);
-        assert!(result.is_ok());
-        let result_value = result.unwrap();
-        assert_eq!(&result_value, expected_html);
+
+        match parse_html_and_inject_data(&data, html) {
+            Ok((true, result_value)) => assert_eq!(&result_value, expected_html),
+            Ok((false, _)) => panic!("should have returned 'true' that inject was successful"),
+            Err(e) => panic!("returned error {e:?}"),
+        }
     }
 
     #[test]
-    fn inject_data_into_html_corrects_invalid_doc() {
+    fn parse_html_and_inject_data_corrects_invalid_doc() {
         let mut data: HashMap<String, String> = HashMap::new();
         data.insert(
             "PUBLIC_API_URL".to_string(),
@@ -167,14 +175,16 @@ mod tests {
         );
         let html = "<html><head><title>Hello World</title></head><h1>Hello World</h1></html>";
         let expected_html = r#"<html><head><title>Hello World</title></head><body data-public_api_url="https://api.example.com/v1" data-public_release_version="v101"><h1>Hello World</h1></body></html>"#;
-        let result = inject_data_into_html(&data, html);
-        assert!(result.is_ok());
-        let result_value = result.unwrap();
-        assert_eq!(&result_value, expected_html);
+
+        match parse_html_and_inject_data(&data, html) {
+            Ok((true, result_value)) => assert_eq!(&result_value, expected_html),
+            Ok((false, _)) => panic!("should have returned 'true' that inject was successful"),
+            Err(e) => panic!("returned error {e:?}"),
+        }
     }
 
     #[test]
-    fn inject_data_into_html_without_public_data_makes_no_diff() {
+    fn parse_html_and_inject_data_without_public_data_makes_no_diff() {
         let mut data: HashMap<String, String> = HashMap::new();
         data.insert(
             "NOT_PUBLIC_VAR".to_string(),
@@ -182,9 +192,11 @@ mod tests {
         );
         let html =
             "<html><head><title>Hello World</title></head><body><h1>Hello World</h1></body></html>";
-        let result = inject_data_into_html(&data, html);
-        assert!(result.is_ok());
-        let result_value = result.unwrap();
-        assert_eq!(&result_value, html);
+
+        match parse_html_and_inject_data(&data, html) {
+            Ok((false, _)) => (),
+            Ok((true, _)) => panic!("should have returned 'false' that inject did not happen"),
+            Err(e) => panic!("returned error {e:?}"),
+        }
     }
 }
