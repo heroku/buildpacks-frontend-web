3bd7e9958dbd63f6e75c7174b01abae11bdf701f
Angel Campbell <angel.campbell@salesforce.com>
2026-01-15 11:06:28 -0800

    add more integration tests for more frameworks

--- buildpacks/website-vite/tests/integration_test.rs
+++ buildpacks/website-vite/tests/integration_test.rs
@@ -27,7 +27,63 @@ fn vite_app() {
 
                 assert_contains!(
                     response_body,
-                    r#"data-public_web_integration_test="runtime-config-via-container-env""#
+                    "vite_app"
+                );
+            },
+        );
+    });
+}
+
+#[test]
+#[ignore = "integration test"]
+fn vite_react_app() {
+    website_nodejs_integration_test("./fixtures/vite_react_app", |ctx| {
+        assert_contains!(ctx.pack_stdout, "Website (Vite)");
+        assert_contains!(ctx.pack_stdout, "Static Web Server");
+        start_container(
+            &ctx,
+            ContainerConfig::new().env(
+                "PUBLIC_WEB_INTEGRATION_TEST",
+                "runtime-config-via-container-env",
+            ),
+            |_container, socket_addr| {
+                let response = retry(DEFAULT_RETRIES, DEFAULT_RETRY_DELAY, || {
+                    ureq::get(&format!("http://{socket_addr}/")).call()
+                })
+                .unwrap();
+                let response_body = response.into_string().unwrap();
+
+                assert_contains!(
+                    response_body,
+                    "vite_react_app"
+                );
+            },
+        );
+    });
+}
+
+#[test]
+#[ignore = "integration test"]
+fn vite_svelte_app() {
+    website_nodejs_integration_test("./fixtures/vite_svelte_app", |ctx| {
+        assert_contains!(ctx.pack_stdout, "Website (Vite)");
+        assert_contains!(ctx.pack_stdout, "Static Web Server");
+        start_container(
+            &ctx,
+            ContainerConfig::new().env(
+                "PUBLIC_WEB_INTEGRATION_TEST",
+                "runtime-config-via-container-env",
+            ),
+            |_container, socket_addr| {
+                let response = retry(DEFAULT_RETRIES, DEFAULT_RETRY_DELAY, || {
+                    ureq::get(&format!("http://{socket_addr}/")).call()
+                })
+                .unwrap();
+                let response_body = response.into_string().unwrap();
+
+                assert_contains!(
+                    response_body,
+                    "vite_svelte_app"
                 );
             },
         );
