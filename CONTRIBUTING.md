5b095ea4eab05395eb2f4371d3001913c23f114b
Angel Campbell <151678097+angeldcampbell@users.noreply.github.com>
2026-01-15 11:33:37 -0800

    add website-vite buildpack (#59)
    * add website-vite buildpack
    
    * update readmes
    
    * format
    
    * update website-nodejs buildpack.toml
    
    * update dependencies
    
    * remove dependency on vite config
    
    * Add new order for vite group to meta-buildpacks buildpack.toml
    
    * update website-vite version to 2.1.0
    
    * Add notes for contributing new buildpacks
    
    * Update README.md
    
    Co-authored-by: Mars Hall <mars@users.noreply.github.com>
    Signed-off-by: Angel Campbell <151678097+angeldcampbell@users.noreply.github.com>
    
    * add more integration tests for more frameworks
    
    * move website vite to top of ordering
    
    * format
    
    ---------
    
    Signed-off-by: Angel Campbell <151678097+angeldcampbell@users.noreply.github.com>
    Co-authored-by: Mars Hall <mars@users.noreply.github.com>

--- CONTRIBUTING.md
+++ CONTRIBUTING.md
@@ -17,3 +17,11 @@ Please follow our [Code of Conduct](CODE_OF_CONDUCT.md).
 
 # License
 By contributing your code, you agree to license your contribution under the terms of our project [LICENSE](LICENSE.txt) and to sign the [Salesforce CLA](https://cla.salesforce.com/sign-cla)
+
+# Adding new framework buildpacks for website-nodjs
+To add support for a nodejs website framework, create a new project in the `buildpacks` directory. Follow the `website-ember` buildpack example and make sure to
+1. Add an implementation test
+1. Update the [buildpack.toml](./meta-buildpacks/website-nodejs/buildpack.toml) in the website-nodejs buildpack to add a new ``[[order]]`` for your buildpack group, including the nodejs and static-web-server buildpacks
+1. Update the [package.toml](./meta-buildpacks/website-nodejs/package.toml) in the website-nodejs buildpack to add your buildpack as a dependency
+1. Update the [Cargo.toml](./Cargo.toml) file to add your new buildpack to the workspace
+1. Update the READMEs in the [root](./README.md) and in [website-nodejs](./meta-buildpacks/website-nodejs/README.md)
---
+++ buildpacks/website-vite/tests/integration_test.rs
@@ -0,0 +1,82 @@
+// Required due to: https://github.com/rust-lang/rust/issues/95513
+#![allow(unused_crate_dependencies)]
+
+use libcnb_test::{assert_contains, ContainerConfig};
+use test_support::{
+    retry, start_container, website_nodejs_integration_test, DEFAULT_RETRIES, DEFAULT_RETRY_DELAY,
+};
+
+#[test]
+#[ignore = "integration test"]
+fn vite_app() {
+    website_nodejs_integration_test("./fixtures/vite_app", |ctx| {
+        assert_contains!(ctx.pack_stdout, "Website (Vite)");
+        assert_contains!(ctx.pack_stdout, "Static Web Server");
+        start_container(
+            &ctx,
+            ContainerConfig::new().env(
+                "PUBLIC_WEB_INTEGRATION_TEST",
+                "runtime-config-via-container-env",
+            ),
+            |_container, socket_addr| {
+                let response = retry(DEFAULT_RETRIES, DEFAULT_RETRY_DELAY, || {
+                    ureq::get(&format!("http://{socket_addr}/")).call()
+                })
+                .unwrap();
+                let response_body = response.into_string().unwrap();
+
+                assert_contains!(response_body, "vite_app");
+            },
+        );
+    });
+}
+
+#[test]
+#[ignore = "integration test"]
+fn vite_react_app() {
+    website_nodejs_integration_test("./fixtures/vite_react_app", |ctx| {
+        assert_contains!(ctx.pack_stdout, "Website (Vite)");
+        assert_contains!(ctx.pack_stdout, "Static Web Server");
+        start_container(
+            &ctx,
+            ContainerConfig::new().env(
+                "PUBLIC_WEB_INTEGRATION_TEST",
+                "runtime-config-via-container-env",
+            ),
+            |_container, socket_addr| {
+                let response = retry(DEFAULT_RETRIES, DEFAULT_RETRY_DELAY, || {
+                    ureq::get(&format!("http://{socket_addr}/")).call()
+                })
+                .unwrap();
+                let response_body = response.into_string().unwrap();
+
+                assert_contains!(response_body, "vite_react_app");
+            },
+        );
+    });
+}
+
+#[test]
+#[ignore = "integration test"]
+fn vite_svelte_app() {
+    website_nodejs_integration_test("./fixtures/vite_svelte_app", |ctx| {
+        assert_contains!(ctx.pack_stdout, "Website (Vite)");
+        assert_contains!(ctx.pack_stdout, "Static Web Server");
+        start_container(
+            &ctx,
+            ContainerConfig::new().env(
+                "PUBLIC_WEB_INTEGRATION_TEST",
+                "runtime-config-via-container-env",
+            ),
+            |_container, socket_addr| {
+                let response = retry(DEFAULT_RETRIES, DEFAULT_RETRY_DELAY, || {
+                    ureq::get(&format!("http://{socket_addr}/")).call()
+                })
+                .unwrap();
+                let response_body = response.into_string().unwrap();
+
+                assert_contains!(response_body, "vite_svelte_app");
+            },
+        );
+    });
+}
