c3f30da95e3f1c4549efc24af3c0355e998e0a34
Angel Campbell <151678097+angeldcampbell@users.noreply.github.com>
2026-01-20 11:57:20 -0800

    Switch logger from commons_ruby to bullet_stream (#64)
    * Switch logger from commons_ruby to bullet_stream
    
    * website-public-html
    
    * static-web-server
    
    * fixups
    
    * linting
    
    * fmt
    
    * Match formatting

--- buildpacks/static-web-server/src/errors.rs
+++ buildpacks/static-web-server/src/errors.rs
@@ -1,11 +1,10 @@
 use crate::BUILDPACK_NAME;
-use commons_ruby::output::build_log::{BuildLog, Logger, StartedLogger};
-use commons_ruby::output::fmt;
-use commons_ruby::output::fmt::DEBUG_INFO;
+use bullet_stream::{global::print, style};
 use indoc::formatdoc;
 use libcnb::TomlFileError;
 use std::fmt::Display;
-use std::io::stdout;
+
+const DEBUG_INFO: &str = "Debug info";
 
 const SUBMIT_AN_ISSUE: &str = "\
 If the issue persists and you think you found a bug in the buildpack then reproduce the issue \
@@ -27,136 +26,112 @@ pub(crate) enum StaticWebServerBuildpackError {
 }
 
 pub(crate) fn on_error(error: libcnb::Error<StaticWebServerBuildpackError>) {
-    let logger = BuildLog::new(stdout()).without_buildpack_name();
     match error {
         libcnb::Error::BuildpackError(buildpack_error) => {
-            on_buildpack_error(buildpack_error, logger);
+            on_buildpack_error(buildpack_error);
         }
-        framework_error => on_framework_error(&framework_error, logger),
+        framework_error => on_framework_error(&framework_error),
     }
 }
 
-fn on_buildpack_error(error: StaticWebServerBuildpackError, logger: Box<dyn StartedLogger>) {
+fn on_buildpack_error(error: StaticWebServerBuildpackError) {
     match error {
-        StaticWebServerBuildpackError::Download(e) => on_download_error(&e, logger),
-        StaticWebServerBuildpackError::Json(e) => on_json_error(&e, logger),
-        StaticWebServerBuildpackError::CannotReadProjectToml(e) => on_toml_error(&e, logger),
+        StaticWebServerBuildpackError::Download(e) => on_download_error(&e),
+        StaticWebServerBuildpackError::Json(e) => on_json_error(&e),
+        StaticWebServerBuildpackError::CannotReadProjectToml(e) => on_toml_error(&e),
         StaticWebServerBuildpackError::CannotParseHerokuWebServerConfiguration(e) => {
-            on_config_error(&e, logger);
+            on_config_error(&e);
         }
         StaticWebServerBuildpackError::CannotWriteCaddyConfiguration(error) => {
-            print_error_details(logger, &error)
-                .announce()
-                .error(&formatdoc! {"
+            print_error_details(&error);
+            print::error(formatdoc! {"
                 Cannot write Caddy configuration for {buildpack_name}
-            ", buildpack_name = fmt::value(BUILDPACK_NAME) });
+            ", buildpack_name = style::value(BUILDPACK_NAME) });
         }
         StaticWebServerBuildpackError::CannotUnpackCaddyTarball(error) => {
-            print_error_details(logger, &error)
-                .announce()
-                .error(&formatdoc! {"
+            print_error_details(&error);
+            print::error(formatdoc! {"
                 Cannot unpack Caddy tarball for {buildpack_name}
-            ", buildpack_name = fmt::value(BUILDPACK_NAME) });
+            ", buildpack_name = style::value(BUILDPACK_NAME) });
         }
         StaticWebServerBuildpackError::CannotCreateCaddyInstallationDir(error) => {
-            print_error_details(logger, &error)
-                .announce()
-                .error(&formatdoc! {"
+            print_error_details(&error);
+            print::error(formatdoc! {"
                 Cannot create Caddy installation directory for {buildpack_name}
-            ", buildpack_name = fmt::value(BUILDPACK_NAME) });
+            ", buildpack_name = style::value(BUILDPACK_NAME) });
         }
         StaticWebServerBuildpackError::CannotCreateCaddyTarballFile(error) => {
-            print_error_details(logger, &error)
-                .announce()
-                .error(&formatdoc! {"
+            print_error_details(&error);
+            print::error(formatdoc! {"
                 Cannot create Caddy tarball file for {buildpack_name}
-            ", buildpack_name = fmt::value(BUILDPACK_NAME) });
+            ", buildpack_name = style::value(BUILDPACK_NAME) });
         }
-        StaticWebServerBuildpackError::BuildCommandFailed(e) => on_build_command_error(&e, logger),
+        StaticWebServerBuildpackError::BuildCommandFailed(e) => on_build_command_error(&e),
         StaticWebServerBuildpackError::CannotCreatWebExecD(error) => {
-            print_error_details(logger, &error)
-                .announce()
-                .error(&formatdoc! {"
+            print_error_details(&error);
+            print::error(formatdoc! {"
                 Cannot create exec.d/web for {buildpack_name}
-            ", buildpack_name = fmt::value(BUILDPACK_NAME) });
+            ", buildpack_name = style::value(BUILDPACK_NAME) });
         }
         StaticWebServerBuildpackError::CannotInstallEnvAsHtmlData(error) => {
-            print_error_details(logger, &error)
-                .announce()
-                .error(&formatdoc! {"
+            print_error_details(&error);
+            print::error(formatdoc! {"
                 Cannot install env-as-html-data (runtime configuration program) for {buildpack_name}
-            ", buildpack_name = fmt::value(BUILDPACK_NAME) });
+            ", buildpack_name = style::value(BUILDPACK_NAME) });
         }
     }
 }
 
-fn on_download_error(
-    error: &libherokubuildpack::download::DownloadError,
-    logger: Box<dyn StartedLogger>,
-) {
-    print_error_details(logger, &error)
-        .announce()
-        .error(&formatdoc! {"
-            Unable to download the static web server for {buildpack_name}. 
-        ", buildpack_name = fmt::value(BUILDPACK_NAME) });
+fn on_download_error(error: &libherokubuildpack::download::DownloadError) {
+    print_error_details(&error);
+    print::error(formatdoc! {"
+        Unable to download the static web server for {buildpack_name}. 
+    ", buildpack_name = style::value(BUILDPACK_NAME) });
 }
 
-fn on_json_error(error: &serde_json::Error, logger: Box<dyn StartedLogger>) {
-    print_error_details(logger, &error)
-        .announce()
-        .error(&formatdoc! {"
-            JSON error from {buildpack_name}. 
-        ", buildpack_name = fmt::value(BUILDPACK_NAME) });
+fn on_json_error(error: &serde_json::Error) {
+    print_error_details(&error);
+    print::error(formatdoc! {"
+        JSON error from {buildpack_name}. 
+    ", buildpack_name = style::value(BUILDPACK_NAME) });
 }
 
-fn on_toml_error(error: &TomlFileError, logger: Box<dyn StartedLogger>) {
-    print_error_details(logger, &error)
-        .announce()
-        .error(&formatdoc! {"
-            TOML error from {buildpack_name}. 
-        ", buildpack_name = fmt::value(BUILDPACK_NAME) });
+fn on_toml_error(error: &TomlFileError) {
+    print_error_details(&error);
+    print::error(formatdoc! {"
+        TOML error from {buildpack_name}. 
+    ", buildpack_name = style::value(BUILDPACK_NAME) });
 }
 
-fn on_config_error(error: &toml::de::Error, logger: Box<dyn StartedLogger>) {
-    print_error_details(logger, &error)
-        .announce()
-        .error(&formatdoc! {"
-            Configuration error from {buildpack_name}. 
-        ", buildpack_name = fmt::value(BUILDPACK_NAME) });
+fn on_config_error(error: &toml::de::Error) {
+    print_error_details(&error);
+    print::error(formatdoc! {"
+        Configuration error from {buildpack_name}. 
+    ", buildpack_name = style::value(BUILDPACK_NAME) });
 }
 
-fn on_build_command_error(error: &std::io::Error, logger: Box<dyn StartedLogger>) {
-    print_error_details(logger, &error)
-        .announce()
-        .error(&formatdoc! {"
-            The custom build command [com.heroku.static-web-server.build] exited with failure. 
-        "});
+fn on_build_command_error(error: &std::io::Error) {
+    print_error_details(&error);
+    print::error(formatdoc! {"
+        The custom build command [com.heroku.static-web-server.build] exited with failure. 
+    "});
 }
 
-fn on_framework_error(
-    error: &libcnb::Error<StaticWebServerBuildpackError>,
-    logger: Box<dyn StartedLogger>,
-) {
-    print_error_details(logger, &error)
-        .announce()
-        .error(&formatdoc! {"
-            {buildpack_name} internal error.
+fn on_framework_error(error: &libcnb::Error<StaticWebServerBuildpackError>) {
+    print_error_details(&error);
+    print::error(formatdoc! {"
+        {buildpack_name} internal error.
 
-            The framework used by this buildpack encountered an unexpected error.
-            
-            If you can't deploy to Heroku due to this issue, check the official Heroku Status page at \
-            status.heroku.com for any ongoing incidents. After all incidents resolve, retry your build.
+        The framework used by this buildpack encountered an unexpected error.
+        
+        If you can't deploy to Heroku due to this issue, check the official Heroku Status page at \
+        status.heroku.com for any ongoing incidents. After all incidents resolve, retry your build.
 
-            {SUBMIT_AN_ISSUE}
-        ", buildpack_name = fmt::value(BUILDPACK_NAME) });
+        {SUBMIT_AN_ISSUE}
+    ", buildpack_name = style::value(BUILDPACK_NAME) });
 }
 
-fn print_error_details(
-    logger: Box<dyn StartedLogger>,
-    error: &impl Display,
-) -> Box<dyn StartedLogger> {
-    logger
-        .section(DEBUG_INFO)
-        .step(&error.to_string())
-        .end_section()
+fn print_error_details(error: &impl Display) {
+    print::bullet(style::important(DEBUG_INFO));
+    print::sub_bullet(error.to_string());
 }
--- buildpacks/website-vite/src/errors.rs
+++ buildpacks/website-vite/src/errors.rs
@@ -1,11 +1,10 @@
 use crate::BUILDPACK_NAME;
-use commons_ruby::output::build_log::{BuildLog, Logger, StartedLogger};
-use commons_ruby::output::fmt;
-use commons_ruby::output::fmt::DEBUG_INFO;
+use bullet_stream::{global::print, style};
 use indoc::formatdoc;
 use std::fmt::Display;
 use std::io;
-use std::io::stdout;
+
+const DEBUG_INFO: &str = "Debug info";
 
 const SUBMIT_AN_ISSUE: &str = "\
 If the issue persists and you think you found a bug in the buildpack then reproduce the issue \
@@ -21,85 +20,71 @@ pub(crate) enum WebsiteViteBuildpackError {
 }
 
 pub(crate) fn on_error(error: libcnb::Error<WebsiteViteBuildpackError>) {
-    let logger = BuildLog::new(stdout()).without_buildpack_name();
     match error {
         libcnb::Error::BuildpackError(buildpack_error) => {
-            on_buildpack_error(buildpack_error, logger);
+            on_buildpack_error(buildpack_error);
         }
-        framework_error => on_framework_error(&framework_error, logger),
+        framework_error => on_framework_error(&framework_error),
     }
 }
 
-fn on_buildpack_error(error: WebsiteViteBuildpackError, logger: Box<dyn StartedLogger>) {
+fn on_buildpack_error(error: WebsiteViteBuildpackError) {
     match error {
-        WebsiteViteBuildpackError::Detect(e) => on_detect_error(&e, logger),
-        WebsiteViteBuildpackError::ReadPackageJson(e) => on_read_package_json_error(&e, logger),
-        WebsiteViteBuildpackError::ParsePackageJson(e) => on_parse_package_json_error(&e, logger),
+        WebsiteViteBuildpackError::Detect(e) => on_detect_error(&e),
+        WebsiteViteBuildpackError::ReadPackageJson(e) => on_read_package_json_error(&e),
+        WebsiteViteBuildpackError::ParsePackageJson(e) => on_parse_package_json_error(&e),
         WebsiteViteBuildpackError::SettingBuildPlanMetadata(e) => {
-            on_toml_serialization_error(&e, logger);
+            on_toml_serialization_error(&e);
         }
     }
 }
 
-fn on_detect_error(error: &io::Error, logger: Box<dyn StartedLogger>) {
-    print_error_details(logger, &error)
-        .announce()
-        .error(&formatdoc! {"
-            Unable to complete buildpack detection.
+fn on_detect_error(error: &io::Error) {
+    print_error_details(&error);
+    print::error(formatdoc! {"
+        Unable to complete buildpack detection.
 
-            An unexpected error occurred while determining if the {buildpack_name} should be \
-            run for this application. See the log output above for more information. 
-        ", buildpack_name = fmt::value(BUILDPACK_NAME) });
+        An unexpected error occurred while determining if the {buildpack_name} should be \
+        run for this application. See the log output above for more information. 
+    ", buildpack_name = style::value(BUILDPACK_NAME) });
 }
 
-fn on_read_package_json_error(error: &io::Error, logger: Box<dyn StartedLogger>) {
-    print_error_details(logger, &error)
-        .announce()
-        .error(&formatdoc! {"
-            Error reading package.json from {buildpack_name}.
-        ", buildpack_name = fmt::value(BUILDPACK_NAME) });
+fn on_read_package_json_error(error: &io::Error) {
+    print_error_details(&error);
+    print::error(formatdoc! {"
+        Error reading package.json from {buildpack_name}.
+    ", buildpack_name = style::value(BUILDPACK_NAME) });
 }
 
-fn on_parse_package_json_error(error: &serde_json::Error, logger: Box<dyn StartedLogger>) {
-    print_error_details(logger, &error)
-        .announce()
-        .error(&formatdoc! {"
-            Error parsing package.json from {buildpack_name}.
-        ", buildpack_name = fmt::value(BUILDPACK_NAME) });
+fn on_parse_package_json_error(error: &serde_json::Error) {
+    print_error_details(&error);
+    print::error(formatdoc! {"
+        Error parsing package.json from {buildpack_name}.
+    ", buildpack_name = style::value(BUILDPACK_NAME) });
 }
 
-fn on_toml_serialization_error(error: &toml::ser::Error, logger: Box<dyn StartedLogger>) {
-    print_error_details(logger, &error)
-        .announce()
-        .error(&formatdoc! {"
-            TOML serialization error from {buildpack_name}. 
-        ", buildpack_name = fmt::value(BUILDPACK_NAME) });
+fn on_toml_serialization_error(error: &toml::ser::Error) {
+    print_error_details(&error);
+    print::error(formatdoc! {"
+        TOML serialization error from {buildpack_name}. 
+    ", buildpack_name = style::value(BUILDPACK_NAME) });
 }
 
-fn on_framework_error(
-    error: &libcnb::Error<WebsiteViteBuildpackError>,
-    logger: Box<dyn StartedLogger>,
-) {
-    print_error_details(logger, &error)
-        .announce()
-        .error(&formatdoc! {"
-            {buildpack_name} internal error.
+fn on_framework_error(error: &libcnb::Error<WebsiteViteBuildpackError>) {
+    print_error_details(&error);
+    print::error(formatdoc! {"
+        {buildpack_name} internal error.
 
-            The framework used by this buildpack encountered an unexpected error.
-            
-            If you can't deploy to Heroku due to this issue, check the official Heroku Status page at \
-            status.heroku.com for any ongoing incidents. After all incidents resolve, retry your build.
+        The framework used by this buildpack encountered an unexpected error.
+        
+        If you can't deploy to Heroku due to this issue, check the official Heroku Status page at \
+        status.heroku.com for any ongoing incidents. After all incidents resolve, retry your build.
 
-            {SUBMIT_AN_ISSUE}
-        ", buildpack_name = fmt::value(BUILDPACK_NAME) });
+        {SUBMIT_AN_ISSUE}
+    ", buildpack_name = style::value(BUILDPACK_NAME) });
 }
 
-fn print_error_details(
-    logger: Box<dyn StartedLogger>,
-    error: &impl Display,
-) -> Box<dyn StartedLogger> {
-    logger
-        .section(DEBUG_INFO)
-        .step(&error.to_string())
-        .end_section()
+fn print_error_details(error: &impl Display) {
+    print::bullet(style::important(DEBUG_INFO));
+    print::sub_bullet(error.to_string());
 }
